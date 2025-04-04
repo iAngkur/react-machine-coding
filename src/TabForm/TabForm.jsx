b9614412b9025559c459ff44032704fb4f214849
angkurmondal1998 <angkurmondal1998@gmail.com>
2025-04-04 18:31:34 +0000

    first commit

---
+++ src/TabForm/TabForm.jsx
@@ -0,0 +1,18 @@
+export default function TabForm() {
+  return (<div>
+
+  </div>);
+}
+
+const styles = {
+    headingContainer: {
+        display: "flex"
+    },
+
+    heading: {
+        padding: "5px",
+        border: "1px solid black",
+    }
+}
+
+
